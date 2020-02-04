d13563b2795d5beaf31bf9b14e48773a56c331a5
onskar <matt.baker988@gmail.com>
2020-02-04 20:32:42 +0000

    Functionalised things.

--- bbc_ccy.py
+++ bbc_ccy.py
@@ -11,13 +11,34 @@ from pathlib import Path
 
 # third party imports
 import pandas as pd
+import requests
 import tweepy
 import yaml
 
 CONFIG = yaml.safe_load(open('config.yml', 'r'))
 
 
-def start_api():
+def cleanse(df_in):
+    # data cleansing
+    df_out = df_in.rename(columns={'Change %': 'Change'})
+    df_out['Change'] = df_out['Change'].str.rstrip('%').astype('float') / 100.0
+    df_out['AbsChange'] = df_out['Change'].abs()
+    return df_out
+
+
+def load_rate_data():
+    file = Path('data/GBP_USD Historical Data.csv')
+    df_raw = pd.read_csv(open(file, 'r', encoding='utf8'))
+    return df_raw
+
+
+def filter_data(df_in):
+    threshold = 0.03
+    df_out = df_in[df_in['AbsChange'] > threshold]
+    return df_out
+
+
+def twitter_api():
     auth = tweepy.OAuthHandler(consumer_key=CONFIG['twitter_login']['consumer_key'],
                                consumer_secret=CONFIG['twitter_login']['consumer_secret'])
     auth.set_access_token(key=CONFIG['twitter_login']['access_key'],
@@ -35,32 +56,32 @@ def search_tweets(date):
     handle = 'BBCBusiness'
     keywords = ['GBP', 'Pound', 'Sterling']
 
-    api = start_api()
+    api = twitter_api()
     tweets = api.search(q=' OR '.join(keywords))
     return [tweet for tweet in tweets]
 
 
 def search_stories(search_date):
-    uri = 'https://www.googleapis.com/customsearch/v1?'
     params = {'key': CONFIG['google']['key'],
               'cx': CONFIG['google']['cx'],
               'q': 'Pound falls',
               'before': search_date + dt.timedelta(days=2),
               'after': search_date + dt.timedelta(days=-1)
               }
+    param_str = '&'.join([f'{k}={v}' for k, v in params.items()])
+    uri = 'https://www.googleapis.com/customsearch/v1?' + param_str
+    r = requests.get(uri)
+    if r.status_code != 200:
+        raise requests.HTTPError(r.json()['message'])
+    hits = r.json()
+    for hit in hits:
+        print(hit)
 
 
 if __name__ == '__main__':
-    file = Path('data/GBP_USD Historical Data.csv')
-    df = pd.read_csv(open(file, 'r', encoding='utf8'))
-
-    # data cleansing
-    df = df.rename(columns={'Change %': 'Change'})
-    df['Change'] = df['Change'].str.rstrip('%').astype('float') / 100.0
-    df['AbsChange'] = df['Change'].abs()
-
-    threshold = 0.03
-    df = df[df['AbsChange'] > threshold]
-
-    df['tweets'] = df['Date'].apply(lambda x: len(search_tweets(x)))
-    print(df)
+    search_stories(dt.date(2017, 1, 17))
+    # df = load_rate_data()
+    # df = cleanse(df)
+    # df = filter_data(df)
+    # df['tweets'] = df['Date'].apply(lambda x: len(search_tweets(x)))
+    # print(df)
